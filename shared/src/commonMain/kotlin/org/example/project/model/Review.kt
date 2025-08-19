3de4b2c02aa6aed84b494f30c49a20fb4c9bf247
SharonBron <7sharonbron7@gmail.com>
2025-08-19 13:55:43 +0300

    change model

--- shared/src/commonMain/kotlin/org/example/project/model/Review.kt
+++ shared/src/commonMain/kotlin/org/example/project/model/Review.kt
@@ -3,10 +3,14 @@ package org.example.project.model
 
 data class Review(
     var id: String = "",
-    var userId: String = "",
     var restaurantId: String = "",
     var rating: Int = 0,
     var comment: String = "",
     var imagePath: String? = null,
-)
 
+    var restaurantName: String = "",
+    var address: String? = null,
+    var latitude: Double? = null,
+    var longitude: Double? = null,
+    var placeId: String? = null
+)
